b1e345a40fb0d4895a1e258887f7dd24b3aee9d3
Giovanni Chajon <giochajon@gmail.com>
2019-08-05 11:46:38 -0600

    doing refresher from Django course

---
+++ djangoCourse/refresher.py
@@ -0,0 +1,14 @@
+age = 45
+
+name = 'Gio' 
+
+print ("my name is {} and I am {}".format (name,age))
+
+# comment
+
+
+
+if age > 18:
+    print ("you are older than 18")
+else:    
+    print ("you are older than 18")
