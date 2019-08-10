41774926c0b494b486ab823c992f9cc2e3c1d706
Giovanni Chajon <giochajon@gmail.com>
2019-08-09 18:09:26 -0600

    started the model

--- djangoCourse/portfolio/portfolio-project/jobs/models.py
+++ djangoCourse/portfolio/portfolio-project/jobs/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
+class Job(models.Model):
+    image = models.ImageField(upload to 'images/')
+    summary = models.CharField(max_lenght=200)
+    
+
 # Create your models here.
