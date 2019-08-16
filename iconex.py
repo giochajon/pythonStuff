94506db44e702697660436d9c72a7b05d5ac3260
Giovanni Chajon <giochajon@gmail.com>
2019-08-15 23:32:28 -0600

    icon test redering

---
+++ iconex.py
@@ -0,0 +1,28 @@
+#Sources
+#https://www.geeksforgeeks.org/python-program-to-print-emojis/
+#https://stackoverflow.com/questions/32934792/printing-icons-in-the-shell
+
+print (u'\u0436\u2620', repr(u'\u0436\u2620'.encode('utf-8')))
+
+# grinning face 
+print("\U0001f600") 
+  
+# grinning squinting face 
+print("\U0001F606") 
+  
+# rolling on the floor laughing 
+print("\U0001F923")
+
+# the complete list is in 
+# https://unicode.org/emoji/charts/full-emoji-list.html
+
+
+print("\N{grinning face}") 
+  
+# slightly smiling face 
+print("\N{slightly smiling face}") 
+  
+# winking face 
+print("\N{winking face}") 
+
+print("\U0001F926") 
