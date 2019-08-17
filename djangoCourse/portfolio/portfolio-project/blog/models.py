22af8c1aa7a8885cdd2d63447640a2612fe59a76
Giovanni Chajon <giochajon@gmail.com>
2019-08-17 17:35:47 -0600

    added the blog model

--- djangoCourse/portfolio/portfolio-project/blog/models.py
+++ djangoCourse/portfolio/portfolio-project/blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+class Blog(models.Model):
+    
+    title = models.CharField(max_length=50)
+    pubdate = models.DateField
+    body = models.TextField
+    image = models.ImageField(upload_to ='images/')    
 # Create your models here.
